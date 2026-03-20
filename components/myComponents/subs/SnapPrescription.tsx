6b0974c6f720307d9577058c9ca790af76465926
Adepoju Ololade Joshua Ilerioluwa <adepojuololade2020@gmail.com>
2026-03-20 22:13:31 +0100

    fixed gemini model error

--- components/myComponents/subs/SnapPrescription.tsx
+++ components/myComponents/subs/SnapPrescription.tsx
@@ -72,7 +72,7 @@ export const SnapPrescription = ({
       } = await axios.get("/api/keys/gemini");
       const genAI = new GoogleGenerativeAI(apiKey);
       const model = genAI.getGenerativeModel(
-        { model: "gemini-1.5-flash" },
+        { model: "gemini-2.5-flash" },
         { apiVersion: "v1" },
       );
 
